4134f70c748a6fd5915f21e721ac5f9b2765a135
rummolprod999 <rummolprod999@gmail.com>
2019-10-02 13:30:44 +0300

    add suspend

--- src/main/java/anbapp/parsers/ParserAbstract.kt
+++ src/main/java/anbapp/parsers/ParserAbstract.kt
@@ -32,7 +32,7 @@ abstract class ParserAbstract {
         val arr = mutableListOf<RoomAnb>()
 
         DriverManager.getConnection(BuilderApp.UrlConnect, BuilderApp.UserDb, BuilderApp.PassDb).use(fun(con: Connection) {
-            val res = con.prepareStatement("SELECT id, url FROM anb_url").executeQuery()
+            val res = con.prepareStatement("SELECT id, url FROM anb_url WHERE suspend = 0").executeQuery()
             while (res.next()) {
                 val id = res.getInt(1)
                 val url = res.getString(2)
